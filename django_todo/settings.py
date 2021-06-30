3cd907d2216016d794c3bdbc031b3520ee9be46b
oksanaokhten <oksanaokhten@yahoo.com>
2021-06-30 15:01:35 +0000

    Setup environment variables and automatic deployments

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 import dj_database_url
 
@@ -21,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-as%@hqsty&he$k(@vmg=he$9r^%0&#pqan6^drf*z+arkqh&aq'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-as%@hqsty&he$k(@vmg=he$9r^%0&#pqan6^drf*z+arkqh&aq')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ckz8780-django-todo-app-sonia.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
@@ -83,7 +84,7 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.parse('postgres://szlxdglyepcekd:f175338f123efa97f34000734e1323f121022503412d98bf7d6d91371bde7e01@ec2-52-5-1-20.compute-1.amazonaws.com:5432/d9oer0qgeleoov')
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
